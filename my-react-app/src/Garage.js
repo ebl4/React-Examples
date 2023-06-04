6e9f45b29737a87f3963b222ab61f878f4e3a76a
Edson <edbdelima@gmail.com>
2023-06-04 17:48:41 -0300

    including Garage and Car components

---
+++ my-react-app/src/Garage.js
@@ -0,0 +1,12 @@
+import Car from './Car.js';
+
+function Garage() {
+    return (
+        <>
+            <h1>Who lives in my Garage?</h1>
+            <Car />
+        </>
+    );
+}
+
+export default Garage;
