7bd5364b8bb0edc91c3be40a68d04de5b4ad807d
Edson <edbdelima@gmail.com>
2023-06-06 20:19:42 -0300

    Using react submit form

--- my-react-app/src/MyForm.js
+++ my-react-app/src/MyForm.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function MyForm() {
+    const [name, setName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`O nome que você entrou foi: ${name}`)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Entre com o seu nome:
-                <input type="text"></input>
+                <input 
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}>
+                </input>
             </label>
+            <input type="submit"></input>
         </form>
     )
 }
