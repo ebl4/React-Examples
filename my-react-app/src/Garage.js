2a1055cd3ac8b889e309aebe8d72830ace953d37
Edson <edbdelima@gmail.com>
2023-06-06 10:19:53 -0300

    utilizando array de objetos carros com key e value

--- my-react-app/src/Garage.js
+++ my-react-app/src/Garage.js
@@ -1,12 +1,16 @@
 import Car from './Car.js';
 
 function Garage() {
-    const cars = ['Ford', 'Audi', 'Ferrari'];
+    const cars = [
+        {id: 1, brand: 'Ford'}, 
+        {id: 2, brand: 'Audi'}, 
+        {id: 3, brand: 'Ferrari'}
+    ];
     return (
         <>
             <h1>Who lives in my Garage?</h1>
             <ul>
-                {cars.map((car) => <Car brand={car}/>)}
+                {cars.map((car) => <Car key={car.id} brand={car.brand}/>)}
             </ul>
         </>
     );
