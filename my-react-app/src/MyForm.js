3ab6997c32590f434deb65d5445eb360cf393c5d
Edson <edbdelima@gmail.com>
2023-06-06 20:45:44 -0300

    example using multiple input fields

--- my-react-app/src/MyForm.js
+++ my-react-app/src/MyForm.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 function MyForm() {
-    const [name, setName] = useState("");
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`O nome que você entrou foi: ${name}`)
+        console.log(inputs);
     }
 
     return (
@@ -14,8 +20,18 @@ function MyForm() {
                 Entre com o seu nome:
                 <input 
                   type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}>
+                  name="username"
+                  value={inputs.username || ""}
+                  onChange={handleChange}>
+                </input>
+            </label>
+            <label>
+                Entre com a sua idade:
+                <input 
+                  type="text"
+                  name="age"
+                  value={inputs.age || ""}
+                  onChange={handleChange}>
                 </input>
             </label>
             <input type="submit"></input>
